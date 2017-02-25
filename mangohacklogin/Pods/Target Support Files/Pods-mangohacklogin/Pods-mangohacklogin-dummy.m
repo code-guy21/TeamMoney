650deaef2a57c8503c7c47561d4f89de90380880
Miguel Chavez <miguelchavez@Miguels-Mac-mini.local>
2017-02-25 15:03:57 -0500

    login

---
+++ mangohacklogin/Pods/Target Support Files/Pods-mangohacklogin/Pods-mangohacklogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mangohacklogin : NSObject
+@end
+@implementation PodsDummy_Pods_mangohacklogin
+@end
---
+++ mangohacklogin/mangohacklogin/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  mangohacklogin
+//
+//  Created by octavio guillermo avila cardet jr on 2/25/17.
+//  Copyright © 2017 Team Get Money. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sign Out", style: .plain, target: self, action: #selector(handleLogout))
+        
+        if FIRAuth.auth()?.currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
+    }
+    
+    func handleLogout() {
+        
+        do {
+            try FIRAuth.auth()?.signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
